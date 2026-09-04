de7f012ce9a84433ddd148af95fc63e1230660b7
AuonT <auon.tippu@gmail.com>
2026-09-04 18:59:57 -0400

    Changed Name

---
+++ IdealGasLaw.py
@@ -0,0 +1,36 @@
+import time
+
+def IdealGasLaw():
+    R = 8.314462618 #constant for ideal gas law
+
+    variables = {
+        1: "Pressure (P)":None,
+        2: "Volume (V)":None,
+        3: "Number of moles (n)":None,
+        4: "Temperature (T)":None
+    }
+    target = float(input("Please enter the unit you would like to solve for:\n\n1. Pressure (P)\n2. Volume (V)\n3. Number of moles (n)\n4. Temperature (T)\n\nPlease enter the number corresponding to your choice: "))
+
+
+
+
+start = input("Would you like to use my Ideal Gas Law Solver? (y/n): ")
+time.sleep(1)
+
+if start != "y" and start != "n":
+    while start != "y" and start != "n":
+        print("Invalid input. Please enter 'y' for yes or 'n' for no.")
+        print()
+        start = input("Would you like to use my Ideal Gas Law Solver? (y/n): ")
+
+
+if start == "y":
+    print ("Hello! Welcome to my Ideal Gas Law Solver!\n\nThis program will help you calculate the missing variable in the Ideal Gas Law equation:\n\nPV = nRT.")
+    print()
+    time.sleep(3)
+    print("Let's start with what variable you would like to solve for!")
+    print()
+    IdealGasLaw()
+else:
+    print("Thank you for using my Ideal Gas Law Solver! Goodbye!")
+    exit()
